d1779ed53b721adc3442d12299d048c1df8a59e9
Jones Vitolo <jsvitolo@gmail.com>
2017-07-04 23:22:50 -0300

    add login and register forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,27 @@ import { AppComponent } from './app.component'
 import { MaterializeModule } from 'angular2-materialize'
 
 import { Angular2TokenService } from 'angular2-token';
+import { FormsModule } from '@angular/forms'
+
 import { HomeComponent } from './home/home.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { AuthDialogComponent } from './auth-dialog/auth-dialog.component'
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ToolbarComponent,
-    AuthDialogComponent
+    AuthDialogComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     AppRoutingModule,
     MaterializeModule
   ],
--- src/app/auth-dialog/auth-dialog.component.ts
+++ src/app/auth-dialog/auth-dialog.component.ts
@@ -13,6 +13,26 @@ export class AuthDialogComponent implements OnInit {
 
   constructor() { }
 
+  onLoginFormResult(e) {
+    if (e.signedIn) {
+      this.closeDialog()
+    } else {
+      alert(e.err.json().errors[0])
+    }
+  }
+
+  onRegisterFormResult(e) {
+    if (e.signedUp) {
+      this.closeDialog()
+    } else {
+      alert(e.err.json().errors.full_messages[0])
+    }
+  }
+
+  closeDialog() {
+    this.modalActions.emit({action: 'modal', params: ['close']})
+  }
+
   openDialog(mode: 'login' | 'register' = 'login') {
     this.authMode = mode
     this.modalActions.emit({ action: 'modal', params: ['open']})
